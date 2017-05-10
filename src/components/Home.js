747a0c3733e2a39bcc77466278771a89407f427f
Vince Scheuerman <vsherms@gmail.com>
2017-05-10 17:37:47 -0600

    fixed home page bug to clear template fields when it loads

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,10 +7,29 @@ import Products from './Products';
 class Home extends React.Component{
   constructor(){
     super();
+    this.resetStore = this.resetStore.bind(this);
   }
 
   componentDidMount() {
     this.props.orderStore.loadProducts(this.props.userStore.pflUser, this.props.userStore.pflPass);
+    this.resetStore();
+  }
+
+  resetStore(){
+    this.props.orderStore.currentOrder = {};
+    this.props.orderStore.fieldInfo = [];
+    this.props.orderStore.firstName = '';
+    this.props.orderStore.lastName = '';
+    this.props.orderStore.companyName = '';
+    this.props.orderStore.address1 = '';
+    this.props.orderStore.address2 = '';
+    this.props.orderStore.city = '';
+    this.props.orderStore.state = '';
+    this.props.orderStore.postalCode = '';
+    this.props.orderStore.countryCode = '';
+    this.props.orderStore.email = '';
+    this.props.orderStore.phone = '';
+    this.props.orderStore.deliveryInfo = {};
   }
 
   render(){
